153058bec4f3786fc79bb8aa86f5056cf92fec92
KDC <69408642+kdctek@users.noreply.github.com>
2025-06-23 10:13:40 +0530

    Update server.js
    Updated the Server

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-console.log(`DIGIPIN API is running and API docs can be found at at http://localhost:${PORT}/api-docs`);
+console.log(`DIGIPIN API is running and API docs can be found at at https://digipin.space:${PORT}/docs`);
 });
